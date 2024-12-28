3b3005a68a886480132d7665b0b5a5254d4d6262
Zmotrovich Nikolay <nzmitrovich@usergate.com>
2024-12-28 13:22:19 +0700

    add many other players

--- check_winrate.py
+++ check_winrate.py
@@ -31,6 +31,7 @@ class Deck:
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-mh', '--main_hand', type=str)
+parser.add_argument('-pn', '--players_num', type=int, default=1)
 parser.add_argument('-oh', '--other_hand', type=str, default=None)
 parser.add_argument('-b', '--bord', type=str, default=None)
 parser.add_argument('-n', '--random_num', type=int, default=100)
@@ -49,14 +50,29 @@ if __name__ == '__main__':
         main_hand = args.main_hand.split(',')
         main_hand = Hand(**{f'card_{i}': Card(*it.split(':')) for i, it in enumerate(main_hand, start=1)})
         
+        other_hands = []
         if args.other_hand is None:
-            other_hand = Hand(
-                deck.get_rand_card(),
-                deck.get_rand_card()
-            )
+            for player_n in range(args.players_num):
+                other_hands.append(
+                    Hand(
+                        deck.get_rand_card(),
+                        deck.get_rand_card()
+                    )
+                )
         else:
-            other_hand = args.other_hand.split(',')
-            other_hand = Hand(**{f'card_{i}': Card(*it.split(':')) for i, it in enumerate(other_hand, start=1)})
+            other_hands_str = args.other_hand.split(';')
+            for hand in other_hands_str:
+                other_hands.append(
+                    Hand(**{f'card_{i}': Card(*it.split(':')) for i, it in enumerate(hand.split(','), start=1)})
+                )
+            
+            while len(other_hands) < args.players_num:
+                other_hands.append(
+                    Hand(
+                        deck.get_rand_card(),
+                        deck.get_rand_card()
+                    )
+                )
 
         if args.bord is None:
             bord = Bord(*[deck.get_rand_card() for _ in range(5)])
@@ -68,9 +84,9 @@ if __name__ == '__main__':
             bord = Bord(*bord_cards)
 
         main_rank, _, _ = calc_max_combination(main_hand, bord)
-        player_rank, _, _ = calc_max_combination(other_hand, bord)
+        players_max_rank = max([calc_max_combination(hand, bord)[0] for hand in other_hands])
 
-        if main_rank >= player_rank:
+        if main_rank >= players_max_rank:
             wins += 1
         else:
             losses += 1
